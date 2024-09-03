0899efe2bd4e2fad1531e781e2a87e087fb4c461
nadinnah <nadinahmedmohamed@gmail.com>
2024-09-03 07:21:29 +0300

    finalize version

--- index.js
+++ index.js
@@ -60,9 +60,37 @@ app.patch("/Edit/:id",(req,res)=>{
 
     if (blogToUpdate) {
         // Update the blog object with the new data from the request body
-        Object.assign(blogToUpdate, req.body);
+        // Object.assign(blogToUpdate, req.body);
         // Redirect to the blog's page to see the updated content
+        // Object.assign(blogToUpdate, {
+        //     title: req.body.title,
+        //     content: req.body.content,
+        //     date: blogToUpdate.date,  // Preserve original date
+        //     time: blogToUpdate.time   // Preserve original time
+        // });
+
+         // Update the blog's title and content with new values from the request body
+         blogToUpdate.title = req.body.title;
+         blogToUpdate.content = req.body.content;
+ 
+         // Update the date and time to the current date and time
+         blogToUpdate.date = new Date().toJSON().slice(0, 10); // Update the date
+         blogToUpdate.time = new Date().toLocaleTimeString('en-US'); // Update the time
+ 
         res.redirect(`/blogwriting/${blogid}`);
+    } else {
+        res.status(404).send("Blog not found");
+    }
+});
+
+app.delete("/Edit/:id",(req,res)=>{
+    var blogid=req.params.id*1; //for it to be a number since id has a string value in blogs
+    var blogToDelete=blogs.find(b=>b.id===blogid); //returns blog object if found
+
+    if (blogToDelete) {
+        blogs.splice(blogToDelete, 1);  //remove the blog at index x and only that blog, leaving the rest of the array unchanged.
+ 
+        res.redirect(`/`);
     } else {
         res.status(404).send("Blog not found");
     }
